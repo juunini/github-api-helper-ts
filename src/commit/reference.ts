85e5adc4496d2415a4aabd1de1c6899761d85183
juunini <juuni.ni.i@gmail.com>
2023-04-23 19:24:43 +0900

    feat: add reference helper function

---
+++ src/commit/reference.ts
@@ -0,0 +1,52 @@
+import { api } from '@/api'
+
+interface GetReferenceProps {
+  owner: string
+  repo: string
+  accessToken: string
+}
+
+interface GetReferenceResponse {
+  ref: string
+  node_id: string
+  url: string
+  object: {
+    sha: string
+    type: string
+    url: string
+  }
+}
+
+export async function getReference ({ owner, repo, accessToken }: GetReferenceProps): Promise<GetReferenceResponse> {
+  return await api({
+    uri: `/repos/${owner}/${repo}/git/ref/heads/main`,
+    accessToken
+  }) as GetReferenceResponse
+}
+
+interface UpdateReferenceProps {
+  commitSha: string
+  owner: string
+  repo: string
+  accessToken: string
+}
+
+export interface UpdateReferenceResponse {
+  ref: string
+  node_id: string
+  url: string
+  object: {
+    sha: string
+    type: string
+    url: string
+  }
+}
+
+export async function updateReference ({ commitSha, owner, repo, accessToken }: UpdateReferenceProps): Promise<UpdateReferenceResponse> {
+  return await api({
+    uri: `/repos/${owner}/${repo}/git/refs/heads/main`,
+    accessToken,
+    method: 'PATCH',
+    body: JSON.stringify({ sha: commitSha })
+  }) as UpdateReferenceResponse
+}
